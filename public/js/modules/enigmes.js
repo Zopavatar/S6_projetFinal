f23b3c8cf1f172b857d56d45fae86a87eea972bb
unknown <Charlotte Landuyt>
2023-01-06 14:01:50 +0100

    instances enigmes

--- public/js/modules/enigmes.js
+++ public/js/modules/enigmes.js
@@ -1,5 +1,3 @@
-export let enigmeArray = ["1","2","3","4"];
-
 export class Enigmes {
     constructor(enigmeIntitu, enigmeRep){
         this.enigmeIntitu = enigmeIntitu;
@@ -11,3 +9,12 @@ export class Enigmes {
         let ask = prompt(enigmeArray[rand].enigmeIntitu);
     }
 }
+
+
+export let enigme_a = new Enigmes("Qu’est-ce qui a des racines que personne ne voit, Qui est plus grand que les arbres, Qui monte, qui monte, Et pourtant ne pousse jamais ?","Une montage");
+export let enigme_b = new Enigmes("Trente chevaux sur une colline rouge; D’abord ils mâchonnent, Puis ils frappent du pied, Ensuite ils restent immobiles.","Des dents");
+export let enigme_c = new Enigmes("Sans voix, il crie, sans ailes, il voltige, sans dents, il mord, sans bouche, il murmure.","le vent");
+export let enigme_d = new Enigmes("Une boite sans charnière, sans clé, sans couvercle: Pourtant à l’intérieur est caché un trésor doré.","un oeuf");
+
+
+export let enigmeArray = [enigme_a,enigme_b,enigme_c,enigme_d];
